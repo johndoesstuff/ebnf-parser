a7b9920c901d7c0505c0879026ffd856d757291a
johndoesstuff <johndoesstuff03@gmail.com>
2024-12-10 12:52:22 -0500

    were getting somewhere

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ enum TokenType {
 	Identifier,
 	Terminal,
 	Operator,
-	EOF,
 }
 
 interface Token {
@@ -15,10 +14,94 @@ interface Token {
 
 class Tokenizer {
 	private position = 0;
+	private tokens: Token[] = [];
+	private regexPattern: { [key: string]: RegExp } = {
+		comment: /\(\*[^*]*\*+(?:[^)*][^*]*\*+)*\)/,
+		whitespace: /^[ \t\n\r]+/,
+		terminal: /".[^"]*"|'.[^"]*'/,
+		identifier: /[A-Za-z][0-9A-Za-z_]*/,
+		operator: /\(:|:\)|\(|\/\)|[=,;\.\|\/!\[\]{}?\(\)]/,
+	};
+	private regexPriority: string[] = [
+		"whitespace",
+		"comment",
+		"terminal",
+		"identifier",
+		"operator",
+	];
 
 	constructor(private input: string) {}
+
+	tokenize(): Token[] {
+		let remaining = this.input;
+
+		while (remaining.length > 0) {
+			let matched = false;
+			this.position = this.input.length - remaining.length;
+			
+			if (this.regexPattern.whitespace.test(remaining)) {
+				const match = remaining.match(this.regexPattern.whitespace);
+				if (match) remaining = remaining.substring(match[0].length);
+				continue;
+			}
+
+			if (this.regexPattern.comment.test(remaining)) {
+				const match = remaining.match(this.regexPattern.comment);
+				if (match) remaining = remaining.substring(match[0].length);
+				continue;
+			}
+
+
+			if (this.regexPattern.terminal.test(remaining)) {
+				const match = remaining.match(this.regexPattern.terminal);
+				if (match) {
+					this.tokens.push({
+						type: TokenType.Terminal,
+						value: match[0],
+						position: this.position,
+					});
+					remaining = remaining.substring(match[0].length);
+				}
+				continue;
+			}
+			
+			if (this.regexPattern.identifier.test(remaining)) {
+				const match = remaining.match(this.regexPattern.identifier);
+				if (match) {
+					this.tokens.push({
+						type: TokenType.Identifier,
+						value: match[0],
+						position: this.position,
+					});
+					remaining = remaining.substring(match[0].length);
+				}
+				continue;
+			}
+
+			if (this.regexPattern.operator.test(remaining)) {
+				const match = remaining.match(this.regexPattern.operator);
+				if (match) {
+					this.tokens.push({
+						type: TokenType.Operator,
+						value: match[0],
+						position: this.position,
+					});
+					remaining = remaining.substring(match[0].length);
+				}
+				continue;
+			}
+
+			throw new Error(`Unexpected token: ${remaining}`);
+		}
+
+		return this.tokens;
+	}
 }
 
 const filePath = process.argv[2];
 const data = fs.readFileSync(filePath, 'utf-8');
-console.log(data);
+
+const tokenizer = new Tokenizer(data);
+const tokens = tokenizer.tokenize();
+
+console.log(tokens);
